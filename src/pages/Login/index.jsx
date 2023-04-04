20427dd86e2aa69a548ce9c03d34f4159dee0467
bonchukovIvan <bonchukooov@gmail.com>
2023-04-04 19:42:02 +0300

    add check auth and logout

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector  } from "react-redux";
+import { Navigate, useNavigate } from "react-router-dom";
 
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
@@ -8,11 +9,11 @@ import Button from "@mui/material/Button";
 import { useForm } from "react-hook-form";
 
 import styles from "./Login.module.scss";
-import { fetchAuth } from "../../redux/slices/auth";
+import { fetchAuth, selectIsAuth } from "../../redux/slices/auth";
 
 export const Login = () => {
+  const isAuth = useSelector(selectIsAuth);
   const dispatch = useDispatch();
-
   const { 
     register, 
     handleSubmit, 
@@ -28,6 +29,10 @@ export const Login = () => {
 
   const onSubmit = (values) => {
    dispatch(fetchAuth(values));
+  }
+
+  if (isAuth) {
+    return <Navigate to="/"/>
   }
 
   return (
